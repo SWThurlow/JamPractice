0775e65c138205eb9d31fc8969b34b3ad99badd9
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-06-14 17:26:19 +0100

    ADD COUNTDOWN TIMER FOR START OF JAM
    Added a timer to countdown until the start of the game jam. The timer
    has been placed in a grid with space for a timer to the end of the jam.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+/*Countdown to start of the jam*/
+const startClock = document.querySelector('.startClock');
+
+const startDate = new Date(2021, 6, 9, 11, 00, 00).getTime();
+
+function updateStartTime(){
+    const now = new Date().getTime();
+    const timeLeft = startDate - now;
+
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
+
+    startClock.textContent = `Days: ${days} Hours: ${hours} Minutes: ${minutes} Seconds: ${seconds}`
+
+    if(timeLeft < 0) clearInterval(startInterval);
+}
+
+let startInterval = setInterval(updateStartTime, 1000);
