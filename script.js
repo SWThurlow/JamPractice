3903b2c1abbb56f7b1768882c4497c34aa944d72
amr1n <amrin0737@gmail.com>
2021-06-15 12:21:21 +0600

    Redesigned the Timer

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 /*Countdown to start of the jam*/
-const startClock = document.querySelector('.startClock');
+const dom =( () => {
+	const hours = document.querySelector(".hours");
+	const days = document.querySelector(".days");
+	const minutes = document.querySelector(".minutes");
+	const seconds = document.querySelector(".seconds");
 
-const startDate = new Date(2021, 6, 9, 11, 00, 00).getTime();
+	return {
+		hours,
+		days,
+		minutes,
+		seconds
+	}
+})();
 
-function updateStartTime(){
+function updateStartTime() {
+	const startDate = new Date(2021, 6, 9, 11, 00, 00).getTime();
     const now = new Date().getTime();
     const timeLeft = startDate - now;
 
     let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
     let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    startClock.textContent = `Days: ${days} Hours: ${hours} Minutes: ${minutes} Seconds: ${seconds}`
+    dom.days.textContent = days; 
+    dom.hours.textContent = hours; 
+    dom.minutes.textContent = minutes; 
+    dom.seconds.textContent = seconds; 
 
     if(timeLeft < 0) clearInterval(startInterval);
 }
